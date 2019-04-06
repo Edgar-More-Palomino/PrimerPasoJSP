7ee77f1043c9db7126c170e210cf8954624ddde1
Edgar More <edmopa216@gmail.com>
2019-04-06 00:43:16 -0500

    ejercicio JSP

---
+++ src/practicas/com/calculosmatematicos/Calculo.java
@@ -0,0 +1,27 @@
+package practicas.com.calculosmatematicos;
+
+public class Calculo {
+	private static int resultado;
+
+	public static int suma_numero(int a, int b) {
+
+		resultado = a + b;
+
+		return resultado;
+	}
+
+	public static int resta_numero(int a, int b) {
+
+		resultado = a - b;
+
+		return resultado;
+	}
+
+	public static int multiplica_numero(int a, int b) {
+
+		resultado = a * b;
+
+		return resultado;
+	}
+
+}
